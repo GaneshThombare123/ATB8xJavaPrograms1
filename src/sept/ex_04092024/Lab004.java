290286c7ff0d611725a27a8692a5247c15e902c7
Ganesh <ganeshthombare0@gmail.com>
2024-10-06 12:30:51 +0530

    add

--- src/sept/ex_04092024/Lab004.java
+++ src/sept/ex_04092024/Lab004.java
@@ -4,5 +4,7 @@ public class Lab004 {
 
     public static void main(String[] args ){
         System.out.println("Hello Ganesh this is your first program and welcome to Java!");
+        System.out.println(2+2);
+        System.out.println(29-14);
     }
 }
