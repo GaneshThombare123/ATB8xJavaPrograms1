39083ab086411fbd3567228d5f612011eb403266
Ganesh <ganeshthombare0@gmail.com>
2024-10-06 15:47:14 +0530

    Add this files as well

---
+++ src/sept/ex_06092024/Lab007.java
@@ -0,0 +1,10 @@
+package sept.ex_06092024;
+
+public class Lab007 {
+    public static void main(String[] ganesh ) {
+
+//        System.out.println("Hi,");-- comment the code
+
+        System.out.println("Hi, Sir");
+    }
+}
---
+++ src/sept/ex_06092024/Lab008.java
@@ -0,0 +1,19 @@
+package sept.ex_06092024;
+
+public class Lab008 {
+
+    public static void main(String[] ganesh ) {
+
+//        System.out.println("Hi,");-- comment the code
+        /* Multi line comments
+         *Author
+         * program to explain
+         *
+         *
+         **/
+// below line of program will executed.it will print Hi, Sir
+
+
+        System.out.println("Hi, Sir");
+    }
+}
