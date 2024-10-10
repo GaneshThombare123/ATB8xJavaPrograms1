6734e81a1164644eb7391a9fb3eb8b374aaa1d5d
Ganesh <ganeshthombare0@gmail.com>
2024-10-10 06:56:19 +0530

    Add this files as well

---
+++ src/sept/ex_16092024/ex_16092024.java
@@ -0,0 +1,15 @@
+package sept.ex_16092024;
+
+public class ex_16092024 {
+    public static void main(String[] args){
+        // = and ==
+
+        int a = 10;  // assigned value to variable
+        boolean b = (10 == 11);  // comparison 2 values
+        System.out.println(a);
+        System.out.println(b);
+
+        // === not present in Java
+
+    }
+}
