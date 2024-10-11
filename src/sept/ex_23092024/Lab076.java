a02e598af6ec3e0c1b0169dd21be5ffdd2fcb1f9
Ganesh <ganeshthombare0@gmail.com>
2024-10-12 05:28:06 +0530

    Add this files as well

--- src/sept/ex_23092024/Lab076.java
+++ src/sept/ex_23092024/Lab076.java
@@ -1,37 +1,39 @@
 package sept.ex_23092024;
 
-public class Lab075 {
+public class Lab076 {
     public static void main(String[] args) {
         //Switch condition
 // Day - 1 to 7 --> 1 mon, 2 tue, 3 wen....7 sun
 
         int day = 1;
         switch (day){
-            default:
-                System.out.println("No idea about this day!!");
-                break;
+
             case 1:
-            System.out.println("Monday");
-            break;
+                System.out.println("Monday");
+
             case 2:
                 System.out.println("Tue");
-                break;
+
             case 3:
                 System.out.println("Wen");
-                break;
+
             case 4:
                 System.out.println("Thur");
                 break;
             case 5:
                 System.out.println("Friday");
-                break;
+                 break;
             case 6:
                 System.out.println("Sat");
-                break;
+                 break;
             case 7:
                 System.out.println("Sun");
                 break;
+            default:
+                System.out.println("No idea about this day!!");
+
         }
         System.out.println("End of the Loop");
     }
+
 }
---
+++ src/sept/ex_23092024/Lab079.java
@@ -0,0 +1,12 @@
+package sept.ex_23092024;
+
+public class Lab079 {
+    // web automation
+    // i will ask which browser you want me run the code
+    //chrome-- execute the chrome
+    //firefox --execute the firefox
+// edge-- edge case
+    String breowser = "chrome";
+
+
+}
