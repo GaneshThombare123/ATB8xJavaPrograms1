d456ed6080f1dab892f24759734e484492250763
Ganesh <ganeshthombare0@gmail.com>
2024-10-13 17:02:24 +0530

    Add this files as well

---
+++ src/Oct/Lab124.java
@@ -0,0 +1,25 @@
+package Oct;
+import sept.Person;
+public class Lab124 {
+    public static void main(String[] args) {
+    // Object class-- OOPS
+        //string class -- ref type
+        //Arrays
+        // for each
+        // 2d nested for loop
+
+        //Class--> blueprint to create Object,
+        // A- Attribute
+        //B- Behaviour
+
+        ATBStudent amit = new ATBStudent();
+        amit age = 35;
+
+        ATBStudent abhishek = new ATBStudent();
+        abhishek.age = 65;
+
+         Person nikunj = new Person();
+
+
+    }
+}
---
+++ src/sept/Person.java
@@ -0,0 +1,11 @@
+package sept;
+
+public class Person {
+    String name;
+
+    void talk(){}
+
+
+
+
+}
